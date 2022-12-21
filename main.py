82d292378ff9d22ca852d0652e4a0771fba6631d
wilsonn-g <wilson-gee@hotmail.com>
2022-12-20 23:58:39 -0800

    remove validators dependency, turn on headless mode, add support for text file, fix bug on #slides and filename bug

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import validators
 import re
 from typing import List
 from time import sleep
@@ -24,22 +23,13 @@ def create_driver_instance() -> webdriver:
     :return: Chrome webdriver instance
     """
     chrome_options = Options()
-    # chrome_options.headless = True
+    chrome_options.headless = True
     chrome_driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=chrome_options
     )
     chrome_driver.maximize_window()
     return chrome_driver
-
-
-def is_valid_url(url: str) -> bool:
-    """
-    Checks if a url is valid
-    :param url: any url
-    :return: True if the url is valid. False otherwise.
-    """
-    return validators.url(url)
 
 
 def scrape_title(driver: webdriver) -> str:
@@ -66,6 +56,7 @@ def get_num_of_slides(driver: webdriver) -> int:
             .perform()
 
         total_slides = driver.find_element(By.ID, "punch-total-slide-count")
+        driver.execute_script("arguments[0].style.display = 'block';", total_slides)  # total_slide element goes to display:none when not scrolling
         return int(re.findall("\d+", total_slides.text)[0])
     except NoSuchElementException:
         print("Error while looking for an element while calculating number of slides")
@@ -82,7 +73,12 @@ def download_slides(slides_url: str, driver: webdriver) -> List:
     :param slides_url: url to the Google Slides presentation
     :return: List of [PIL.Images], each element representing a slide in the presentation
     """
-    driver.get(slides_url)
+    try:
+        driver.get(slides_url)
+    except Exception:
+        print(f'Ran into exception while trying to download slide {slides_url}')
+        return []
+
     num_slides = get_num_of_slides(driver)
     slides = []
     ActionChains(driver)\
@@ -117,30 +113,63 @@ def screenshot_slide(driver: webdriver) -> Image:
     return Image.open(buf).convert("RGB")
 
 
+def sanitize_filename(filename: str) -> str:
+    """
+    Removes any OS reserved characters in a string
+    :param filename: file name to be sanitized
+    :return: A valid filename string
+    """
+    forbidden_chars = '"*\\/\'.|?:<>'
+    sanitized_output = ''.join([x if x not in forbidden_chars else '' for x in filename])
+
+    return sanitized_output
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="SlidesExporter",
-        usage="main.py [-h] [--slides] [FILE/URL]\n Example: python main.py "
-              "--slides https://docs.google.com/presentation/d/1pA4QO0WEVGbTMpmKBV_1n3458PKxtvvFzDKZi_rsgAo\n "
+        usage="main.py [-h] [--url] [SLIDES_URL] [--slides] [FILE]\n Example: python main.py "
+              "--url https://docs.google.com/presentation/d/1pA4QO0WEVGbTMpmKBV_1n3458PKxtvvFzDKZi_rsgAo\n "
               "python main.py --slides ./slides-list.txt",
         description="Exports Google Slides presentations as a PDF even if the exporting is disabled by the owner",
     )
 
     parser.add_argument(
-        "--slides",
-        help="URL link to the Google Slides presentation you want to copy or path to a text"
-             "file containing multiple Slides URLs (each have to be on a newline)",
+        "--url",
+        help="URL link to the Google Slides presentation you want to copy",
         type=str,
-        required=True,
+    )
+
+    parser.add_argument(
+        "--slides",
+        help="path to a text file containing all Slides URLs (each have to be on a newline)",
+        type=str,
     )
 
     args = parser.parse_args()
 
+    if args.slides is None and args.url is None:
+        parser.error("Supply either a google slides URL or a new-line delimited file with multiple slide urls")
+
     driver = create_driver_instance()
 
-    if is_valid_url(args.slides):
-        slide_deck = download_slides(args.slides, driver)
-        title = scrape_title(driver)
-        slide_deck[0].save(f"{title}.pdf", save_all=True, append_images=slide_deck[1:])
+    presentations_to_download = []
+
+    if args.url:
+        presentations_to_download.append(args.url)
+
+    if args.slides:
+        try:
+            with open(args.slides, "r") as file:
+                presentations_to_download = presentations_to_download + file.read().splitlines()
+        except OSError:
+            print(f"Failed to open file {args.slides}")
+            parser.print_help()
+
+    for presentation in presentations_to_download:
+        slide_deck = download_slides(presentation, driver)
+        title = sanitize_filename(scrape_title(driver))
+        if len(slide_deck) > 0:
+            slide_deck[0].save(f"{title}.pdf", save_all=True, append_images=slide_deck[1:], quality=95)
 
     driver.close()
